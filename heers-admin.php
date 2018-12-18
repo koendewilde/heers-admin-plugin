b5dd9b5527628ebd1f16375ef66c3381102deb1d
Koen de Wilde <koenstwerk@gmail.com>
2018-12-18 11:05:23 +0100

    Changed footer text

--- heers-admin.php
+++ heers-admin.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Heers Admin
  * Plugin URI:      https://www.heers.nl
  * Description:     Heers.nl support plugin
- * Version:         1.0.0
+ * Version:         1.0.1
  * Author:          Koen de Wilde
  * Author URI:      https://koendewilde.com
  */
@@ -12,7 +12,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-  
+// add support button  
 function kdw_admin_support_button( $wp_admin_bar ) {
 	$args = array(
 		'id'    => 'heers-button-support',
@@ -29,12 +29,8 @@ function kdw_admin_support_button( $wp_admin_bar ) {
 }
 add_action( 'admin_bar_menu', 'kdw_admin_support_button', 999 );
 
-
-add_action( 'admin_enqueue_scripts', 'custom_wp_toolbar_css_admin' );
-add_action( 'wp_enqueue_scripts', 'custom_wp_toolbar_css_admin' );
- 
-function custom_wp_toolbar_css_admin() {
-    
+function kdw_custom_wp_toolbar_css_admin() {
+	
     if ( is_admin_bar_showing() ) {
          
         $css = '#wpadminbar .heers-button-support a.ab-item:before { content: \'\\f242\'; top: 2px;}';
@@ -44,12 +40,15 @@ function custom_wp_toolbar_css_admin() {
         wp_add_inline_style( 'toolbar-css', $css );
         
     }
+	
 }
+add_action( 'admin_enqueue_scripts', 'kdw_custom_wp_toolbar_css_admin' );
+add_action( 'wp_enqueue_scripts', 'kdw_custom_wp_toolbar_css_admin' ); 
 
 
 // change footer text
 function kdw_change_admin_footer(){
-	 echo '<span id="footer-note">WordPress <a href="https://www.heers.nl/" target="_blank">Heerst</a>.</span>';
+	 echo '<span id="footer-note">Deze website is gemaakt door <a href="https://www.heers.nl/" target="_blank">Heers</a>.</span>';
 	}
 add_filter('admin_footer_text', 'kdw_change_admin_footer');
 
